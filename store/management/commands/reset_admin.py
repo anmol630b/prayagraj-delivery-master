488eeaaf656eb1bfcfb4c63454de62a6cc9ad5ab
Anmol yadav <your_email@gmail.com>
2026-03-03 01:11:45 +0530

    Fix reset_admin command

--- store/management/commands/reset_admin.py
+++ store/management/commands/reset_admin.py
@@ -1,4 +1,3 @@
-
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 
@@ -14,8 +13,3 @@ class Command(BaseCommand):
         except User.DoesNotExist:
             User.objects.create_superuser('admin', 'admin@prayagraj.com', 'Admin@1234')
             self.stdout.write('Superuser created!')
-```
-
-Phir start command Railway mein update karo:
-```
-python manage.py collectstatic --noinput && python manage.py migrate && python manage.py reset_admin && gunicorn backend.wsgi:application --bind 0.0.0.0:$PORT
